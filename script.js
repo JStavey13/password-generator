7ee75609c73054aecd1dcd052d094d2cfbc527d3
Jartius <JStavey13@users.noreply.github.com>
2023-01-03 18:38:30 -0500

    added possible characters

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const upperCase = toUpperCase(lowerCase);
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const spclChar = ["!", "#", "$", "^", "&"];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
