7ad1e627330e26612e3e61e95b9e30efc644a107
Jartius <JStavey13@users.noreply.github.com>
2023-01-03 18:41:25 -0500

    updated possile character code

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 var generateBtn = document.querySelector("#generate");
 
 const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-const upperCase = toUpperCase(lowerCase);
+const upperCase = lowerCase.map(element => {
+  return element.toUpperCase();
+});
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const spclChar = ["!", "#", "$", "^", "&"];
 
